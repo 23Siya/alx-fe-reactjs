ee59f30ff946fbbd3a0ac67f5f98f0e8d94a4a1b
23Siya <156370624+23Siya@users.noreply.github.com>
2025-02-18 23:41:31 +0200

    Add files via upload

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,7 +8,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
+    <div>
+            <WelcomeMessage />
+        </div>
+        
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +34,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      
     </>
+    
   )
 }
 
