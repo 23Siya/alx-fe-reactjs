c5528a7d239cce5f73eb3fbeb6001896cbf109ea
23Siya <156370624+23Siya@users.noreply.github.com>
2025-02-19 12:57:32 +0200

    Add files via upload

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 function Footer (){
     return(
         <footer>
+            <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}></div>
   <p>© 2023 City Lovers</p>
 </footer>
     );
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,6 +2,7 @@ function MainContent()
 {
     return(
         <main>
+          <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}></div>
         <p>I love to visit New York, Paris, and Tokyo.</p>
       </main>
     );
