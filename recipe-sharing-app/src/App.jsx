9e3a72b3a9659e97b45da04e770ed3ea5b0068c7
23Siya <156370624+23Siya@users.noreply.github.com>
2025-02-27 15:58:41 +0200

    Add files via upload

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,22 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import RecipeList from './Components/RecipeList.jsx';
-import AddRecipeForm from './Components/AddRecipeForm.jsx';
-
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
 
 function App() {
-  
-
   return (
- 
-        <div>
-      <h1>Recipe Sharing App</h1>
+    <div className="app">
+      <h1>Recipe Sharing</h1>
       <AddRecipeForm />
       <RecipeList />
     </div>
-   
   );
 }
 
