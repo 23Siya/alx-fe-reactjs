ff6ea5682ebc59008b17ca6fdcad802d2201eabe
23Siya <156370624+23Siya@users.noreply.github.com>
2025-03-25 12:34:17 +0200

    Add files via upload

---
+++ recipe-sharing-platform/src/components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    
+    <div className="text-red-500 text-3xl font-bold">
+    If this text is red, Tailwind is working!
+  </div>
+    
+  )
+}
+
+export default App
