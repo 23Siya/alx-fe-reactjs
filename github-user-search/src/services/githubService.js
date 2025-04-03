07897847d778f16599930577e6b9e69e13eaa49a
23Siya <156370624+23Siya@users.noreply.github.com>
2025-04-03 10:38:54 +0200

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
-export const fetchUserData = async (username) => {
-  const API_URL = `https://api.github.com/users/${username}`;
+export const fetchUsers = async (username, location, minRepos) => {
+  let query = "";
+
+  if (username) query += `${username} `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>${minRepos} `;
+
+  const API_URL = `https://api.github.com/search/users?q=${query.trim()}`;
 
   try {
     const response = await axios.get(API_URL, {
@@ -11,6 +17,7 @@ export const fetchUserData = async (username) => {
     });
     return response.data;
   } catch (error) {
-    throw new Error("User not found");
+    console.error("Error fetching users:", error);
+    throw new Error("Could not fetch GitHub users.");
   }
 };
