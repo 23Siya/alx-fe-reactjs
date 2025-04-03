229f4f2d950509b5ac4b472beef0ebafb54f29f4
23Siya <156370624+23Siya@users.noreply.github.com>
2025-04-03 10:16:17 +0200

    Add files via upload

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,23 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Search from "./components/Search";
 import './App.css'
 
 function App() {
   const [search, setSearch] = useState("");
 
   return (
-    <div className="container">
-      <h1>GitHub User Search</h1>
-      <input
-        type="text"
-        placeholder="Search GitHub username..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <button>Search</button>
+   
+      <div className="App">
+      <Search />
     </div>
-  );
+      );
 }
 
 export default App
